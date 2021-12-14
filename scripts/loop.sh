ee8693cb3f66b4729b9e94986f34fc6770ed60a4
Sanjay Kumar <Sanjayrohilla@gmail.com>
2021-12-15 09:57:05 +1100

    while updated

--- scripts/loop.sh
+++ scripts/loop.sh
@@ -1,6 +1,6 @@
 counter = 1
-while [ $counter -lt 5 ] ; do
-        echo "while loop $counter"
+while [ $counter -lt 5 ]; do
+        echo "while loop counter"
         counter=$((counter+1)) 
    done 
 
