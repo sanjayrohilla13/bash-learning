15411392c4f5b7ab1b3e0574db09d427af75c7d0
Chandrika <chander.kala15@gmail.com>
2021-12-14 15:31:56 +1100

    If then else structure

---
+++ scripts/if-then-else.sh
@@ -0,0 +1,19 @@
+age=10
+name="Yashvi"
+
+echo "this is in ifelse_loop2 pgm"
+if [ "$age" -ge 13 -a "$name" = "Yashvi" ];
+then
+echo "child is teen "
+else
+echo "child is not teen yet"
+fi
+
+#age=10
+#name="Yashvi"
+# if [ age <=13 ];
+#then
+#echo "child is teen yet"
+#else
+#echo "child is teen"
+#fi
