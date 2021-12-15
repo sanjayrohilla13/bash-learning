baaf963b0a2c47f86884035c4a33ed2af446a11c
Sanjay Kumar <Sanjayrohilla@gmail.com>
2021-12-15 11:24:37 +1100

    x

--- scripts/for-loop.sh
+++
@@ -1,4 +0,0 @@
-for i in 1 2 3 4 5 6
-  do 
-     echo "Welcome $i times"
- done
---
+++ scripts/for-select-loop.sh
@@ -0,0 +1,9 @@
+for i in 1 2 3 4 5 6
+  do 
+     echo "Welcome $i times"
+ done
+# select 
+select i in 1 2 3 4 5
+do
+echo "Select $i"
+done
